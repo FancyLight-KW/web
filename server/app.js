5b0014901a0008af8e59630cf9e22fc6c639270c
bingoring <ritch1530@gmail.com>
2021-02-25 05:59:39 +0900

    feature/ 로그인, 회원가입 구현

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
