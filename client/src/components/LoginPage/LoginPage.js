3234e2704472e2a4d994db00b71d58f5bce904ea
woojerry <woojerry@naver.com>
2021-02-27 15:09:45 +0900

    Redux for login/register added

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../actions/user";
+
 import axios from "axios";
 
 function LoginPage() {
-  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/login").then((response) => {
+  //     if (response.data.loggedIn == true) {
+  //       setLoginStatus(response.data.user[0].username);
+  //     }
+  //   });
+  // }, []);
+
+  const dispatch = useDispatch();
+  //axios.defaults.withCredentials = true;
 
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
@@ -16,36 +28,50 @@ function LoginPage() {
   };
 
   const onSubmitHandler = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // 새로고침 방지
 
-    axios
-      .post("http://localhost:5000/login", {
-        email: Email,
-        password: Password,
-      })
-      .then((response) => {
-        if (response.data.message) {
-          alert("이메일 혹은 비밀번호가 다릅니다.");
-        } else {
-          console.log(response);
-          return alert(response.data[0].User_ID);
-        }
-      });
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.message) {
+        alert("이메일 혹은 비밀번호가 다릅니다.");
+      } else {
+        alert(response.payload[0].User_ID);
+      }
+    });
   };
 
-  const login = () => {
-    // axios.post('http://localhost:5000/login', {
-    //   email: Email,
-    //   password: Password,
-    // }).then((response) => {
-    //   if(response.data.message){
-    //     alert("이메일 혹은 비밀번호가 다릅니다.");
-    //   } else {
-    //     console.log(response);
-    //     return alert(response.data[0].User_ID);
-    //   }
-    // });
-  };
+  // axios
+  //   .post("http://localhost:5000/login", {
+  //     email: Email,
+  //     password: Password,
+  //   })
+  //   .then((response) => {
+  //     if (response.data.message) {
+  //       alert("이메일 혹은 비밀번호가 다릅니다.");
+  //     } else {
+  //       console.log(response);
+  //       return alert(response.data[0].User_ID);
+  //     }
+  //   });
+  // };
+
+  // const login = () => {
+  //   // axios.post('http://localhost:5000/login', {
+  //   //   email: Email,
+  //   //   password: Password,
+  //   // }).then((response) => {
+  //   //   if(response.data.message){
+  //   //     alert("이메일 혹은 비밀번호가 다릅니다.");
+  //   //   } else {
+  //   //     console.log(response);
+  //   //     return alert(response.data[0].User_ID);
+  //   //   }
+  //   // });
+  // };
 
   return (
     <div
