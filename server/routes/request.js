2ea25a7e9b4c388f51094ae6760e3b927eefc4fd
jrhong95 <jrhong95@gmail.com>
2021-03-06 23:48:44 +0900

    Add request method

--- server/routes/request.js
+++ server/routes/request.js
@@ -5,4 +5,6 @@ const request = require("../controllers/Request/Requests.controller");
 router.post("/newRequest/", request.create);
 router.get("/getAllRequest/", request.findAll);
 router.get("/getRequest/:userId", request.findOne);
+router.put("/updateRequest/:requestId", request.update);
+router.delete("/deleteRequest/:requestId", request.delete);
 module.exports = router;
