eb47198266ef44afaf8a09550bbe4673e39da581
jrhong95 <jrhong95@gmail.com>
2021-03-09 15:54:17 +0900

    로그인 변경

--- server/controllers/Users/Users.login.controller.js
+++ server/controllers/Users/Users.login.controller.js
@@ -1,4 +1,5 @@
 const models = require("../../models");
+const encrypt = require("./encrypt");
 
 // 로그인
 exports.login = (req, res) => {
@@ -8,18 +9,35 @@ exports.login = (req, res) => {
     },
   })
     .then((result) => {
-      if (req.session.user) {
-        console.log("이미 로그인 되어 있음");
-      } else {
-        req.session.user = {
-          id: req.body.User_id,
-          name: req.body.User_name,
-          authorized: true,
-        };
-        console.log("세션 생성 완료.");
-      }
+      if (
+        encrypt.isPasswordSame(req.body.User_password, result.User_password)
+      ) {
+        // Check password
+        if (req.session.user) {
+          console.log("이미 로그인 되어 있음");
+        } else {
+          req.session.user = {
+            id: req.body.User_id,
+            name: req.body.User_name,
+            authorized: true,
+          };
+          console.log("세션 생성 완료.");
 
-      res.send(result);
+          res.send({
+            User_id: result.User_id,
+            User_name: result.User_name,
+            //User_lastlogin:
+            User_position: result.User_position,
+            resultCode: 0,
+          });
+        }
+      } else {
+        console.log("Login: Invalid password");
+        res.send({
+          message: "Invalid user",
+          resultCode: 1,
+        });
+      }
     })
     .catch((err) => {
       res.send(err);
