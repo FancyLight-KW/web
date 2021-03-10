80076069c2a3adeeb24ea34b6db5a357a1f02dd3
jrhong95 <jrhong95@gmail.com>
2021-03-10 11:49:12 +0900

    change request router

--- server/routes/request.js
+++ server/routes/request.js
@@ -7,4 +7,7 @@ router.get("/getAllRequest/", request.findAll);
 router.get("/getRequest/:userId", request.findOne);
 router.put("/updateRequest/:requestId", request.update);
 router.delete("/deleteRequest/:requestId", request.delete);
+
+router.get("/searchRequest/:searchParam/:keyword", request.findRequest);
+//router.get("/getTotalRequest/", request.get);
 module.exports = router;
