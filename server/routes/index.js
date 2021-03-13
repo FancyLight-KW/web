5a57d2a7f5b26e1c6459eb69795a49dc7950db2f
jrhong95 <jrhong95@gmail.com>
2021-03-13 16:24:49 +0900

    홈화면에 CSR상태의 개수 확인

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controllers/Users/Users.controller");
 const login = require("../controllers/Users/Users.login.controller");
+const index = require("../controllers/index/index.controller");
 
 router.post("/register", user.create);
 router.post("/login", login.login);
 router.get("/logout", login.logout);
 
-/* GET home page. */
 // 여기에 쿠키, 세션정보 확인 해야할 듯
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/", index.countCSR_STATUS);
 
 module.exports = router;
