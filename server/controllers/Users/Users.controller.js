e945154182b943b69b9e96d01d925935eb4899ca
jrhong95 <jrhong95@gmail.com>
2021-03-22 21:02:36 +0900

    유저 리스트 추가

--- server/controllers/Users/Users.controller.js
+++ server/controllers/Users/Users.controller.js
@@ -58,6 +58,17 @@ exports.findAll = (req, res) => {
     });
 };
 
+exports.userList = (req, res) => {
+  models.Users.findAll({
+    attributes: ["User_id"],
+  })
+    .then((result) => {
+      res.send(Array.from(result, (x) => x["User_id"]));
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 // 유저 id로 회원 찾음
 exports.findOne = (req, res) => {
   models.Users.findOne({
