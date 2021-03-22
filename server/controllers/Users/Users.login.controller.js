7fe572e8c0225c1e4e665152ce681e79686ce238
jrhong95 <jrhong95@gmail.com>
2021-03-22 14:46:41 +0900

    로그인 롤백

--- server/controllers/Users/Users.login.controller.js
+++ server/controllers/Users/Users.login.controller.js
@@ -1,45 +1,45 @@
 const models = require("../../models");
 const encrypt = require("../../config/password.encrypt");
-const moment = require('../../config/moment.config');
+const moment = require("../../config/moment.config");
 
 // 로그인
-exports.login = (req, res) => {
-  let body = req.body;
-  console.log("login");
-  models.Users.findOne({
-    where: {
-      User_id: body.User_id,
-    },
-  })
-    .then((result) => {
-      if (
-        result &&
-        encrypt.isPasswordSame(body.User_password, result.User_password)
-      ) {
-        console.log("Login: succeeded");
-        // Check password
-        res.send({
-          User_id: result.User_id,
-          User_name: result.User_name,
-          //User_lastlogin:
-          User_position: result.User_position,
-          resultCode: 0,
-        });
-        return done(null, result); 
-      } else {
-        console.log("Login: Invalid password");
-        res.send({
-          message: "Invalid user",
-          resultCode: 1,
-        });
-        return done(false, null, { message: 'Incorrect user info.' });
-      }
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-};
-/*
+// exports.login = (req, res) => {
+//   let body = req.body;
+//   console.log("login");
+//   models.Users.findOne({
+//     where: {
+//       User_id: body.User_id,
+//     },
+//   })
+//     .then((result) => {
+//       if (
+//         result &&
+//         encrypt.isPasswordSame(body.User_password, result.User_password)
+//       ) {
+//         console.log("Login: succeeded");
+//         // Check password
+//         res.send({
+//           User_id: result.User_id,
+//           User_name: result.User_name,
+//           User_lastlogin: result.User_lastlogin,
+//           User_position: result.User_position,
+//           resultCode: 0,
+//         });
+//         return done(null, result);
+//       } else {
+//         console.log("Login: Invalid password");
+//         res.send({
+//           message: "Invalid user",
+//           resultCode: 1,
+//         });
+//         return done(false, null, { message: 'Incorrect user info.' });
+//       }
+//     })
+//     .catch((err) => {
+//       res.send(err);
+//     });
+// };
+
 exports.login = (req, res) => {
   models.Users.findOne({
     where: {
@@ -51,35 +51,25 @@ exports.login = (req, res) => {
         encrypt.isPasswordSame(req.body.User_password, result.User_password)
       ) {
         // Check password
-        if (req.session.user) {
-          console.log("이미 로그인 되어 있음");
-        } else {
-          req.session.user = {
-            id: body.User_id,
-            authorized: true,
-            name: body.User_name,
-          };
-          console.log("세션 생성 완료.");
-          const nowDate = moment().format("YYYY-MM-DD HH:mm:ss");
-          models.Users.update(
-            {
-              User_lastlogin: nowDate,
-            },
-            {
-              where: {
-                User_id: result.User_id,
-              },
-            }
-          );
-
-          res.send({
-            User_id: result.User_id,
-            User_name: result.User_name,
+        const nowDate = moment().format("YYYY-MM-DD HH:mm:ss");
+        models.Users.update(
+          {
             User_lastlogin: nowDate,
-            User_position: result.User_position,
-            resultCode: 0,
-          });
-        }
+          },
+          {
+            where: {
+              User_id: result.User_id,
+            },
+          }
+        );
+
+        res.send({
+          User_id: result.User_id,
+          User_name: result.User_name,
+          User_lastlogin: nowDate,
+          User_position: result.User_position,
+          resultCode: 0,
+        });
       } else {
         console.log("Login: Invalid password");
         res.send({
@@ -92,16 +82,15 @@ exports.login = (req, res) => {
       res.send(err);
     });
 };
-*/
 // 로그아웃
 exports.logout = (req, res) => {
   req.session.destroy((err) => {
-    if(err){
+    if (err) {
       console.log(err);
-      res.send('session not destroyed');
-    }else{
-      console.log('session successfully destroyed');
-      res.send('session destroyed');
+      res.send("session not destroyed");
+    } else {
+      console.log("session successfully destroyed");
+      res.send("session destroyed");
     }
   }); // 세션 삭제
   res.clearCookie("key"); // 세션 쿠키 삭제
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controllers/Users/Users.controller");
 const login = require("../controllers/Users/Users.login.controller");
-const passport = require('passport');
+const passport = require("passport");
 const index = require("../controllers/index/index.controller");
 
 //라우팅
 router.post("/register", user.create);
+router.post("/login", login.login);
 router.get("/logout", login.logout);
 
 // router.get("/login", (req, res) => {
@@ -16,27 +17,27 @@ router.get("/logout", login.logout);
 //   res.render('login');
 // })
 
-router.post('/login',(req, res, next) => {
-  if(req.isAuthenticated()){
-    console.log("Already logined")
-  }
-  else{
-    console.log("new login!");
-    passport.authenticate('local',{
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true,
+router.post(
+  "/login",
+  (req, res, next) => {
+    if (req.isAuthenticated()) {
+      console.log("Already logined");
+    } else {
+      console.log("new login!");
+      passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+        failureFlash: true,
       })(req, res, next);
     }
-}, (req, res) => {
-  if(req.isAuthenticated()){
-    console.log("auth success")
-    console.log(req.user)
+  },
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      console.log("auth success");
+      console.log(req.user);
+    } else [console.log("auth fail")];
   }
-  else[
-    console.log("auth fail")
-  ]
-});
+);
 
 // router.post('/login', passport.authenticate('local',{
 //   successRedirect: '/',
@@ -52,17 +53,16 @@ router.post('/login',(req, res, next) => {
 /* GET home page. */
 // 여기에 쿠키, 세션정보 확인 해야할 듯
 router.get("/", (req, res) => {
-  console.log("auth: " + req.isAuthenticated())
-  if(req.isAuthenticated()){
+  console.log("auth: " + req.isAuthenticated());
+  if (req.isAuthenticated()) {
     res.send({
-      session: req.session
+      session: req.session,
     });
-  }
-  else{
+  } else {
     res.send(req.session.passport);
   }
 });
 
-router.get("/getCSR_COUNT", index.countCSR_STATUS);
+router.get("/csrstatus", index.countCSR_STATUS);
 
 module.exports = router;
