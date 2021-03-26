5e56be845e08484775b3cff82df085dfa6aad17b
jrhong95 <jrhong95@gmail.com>
2021-03-26 23:31:55 +0900

    dd

--- server/routes/request.js
+++ server/routes/request.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const request = require("../controllers/Request/Requests.controller");
 const upload = require("../config/fileupload");
 
-router.post("/newRequest", upload.single("imagefile"), request.create);
+router.post(
+  "/newRequest",
+  (req, res, next) => {
+    console.log(JSON.stringify(req));
+    upload.single("imagefile");
+    next();
+  },
+  request.create
+);
 router.get("/getAllRequest", request.findAll);
 router.get("/searchRequest", request.findRequest);
 router.put("/updateRequest/:requestId", request.update);
