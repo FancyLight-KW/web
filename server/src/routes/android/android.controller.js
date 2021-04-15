5a2b4104d46d5633c2df760fef60792d138890f1
jrhong95 <jrhong95@gmail.com>
2021-04-15 16:49:04 +0900

    안드로이드 메세지 보내기

--- server/src/routes/android/android.controller.js
+++ server/src/routes/android/android.controller.js
@@ -1,4 +1,6 @@
 const models = require("../../DB/models");
+const FCM_Admin = require("firebase-admin");
+const { response } = require("../../../app");
 
 exports.addDeviceID = (req, res) => {
   models.Devices.create({
@@ -11,5 +13,40 @@ exports.addDeviceID = (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).send(err);
+    });
+};
+
+exports.sendMessageToDevice = (req, res) => {
+  models.Devices.findOne({
+    where: {
+      DEVICE_USER_ID: req.user.User_id,
+    },
+  })
+    .then((result) => {
+      const targetToken = result.DEVICE_ID;
+      let msg = {
+        notification: {
+          title: "이동기 병신",
+          body: "ㅋㅋㅋ",
+        },
+        data: {
+          test: "안녕",
+        },
+        token: targetToken,
+      };
+
+      FCM_Admin.send(msg)
+        .then((result) => {
+          console.log(result);
+          res.send("send success");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send("send fail");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
     });
 };
--- server/src/routes/android/android.js
+++ server/src/routes/android/android.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const android = require("./android.controller");
 
 router.post("/", android.addDeviceID);
+router.get("/", android.sendMessageToDevice);
 
 module.exports = router;
