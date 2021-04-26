04f4ff50e93bdf5f8612c7852fc454d4f3d39ca6
jrhong95 <jrhong95@gmail.com>
2021-04-26 22:37:19 +0900

    요청 받아올 때 이름 가져오기

--- server/src/DB/models/Users.js
+++ server/src/DB/models/Users.js
@@ -1,49 +1,61 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Users', {
-    User_id: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+
+module.exports = function (sequelize, DataTypes) {
+  const Users = sequelize.define(
+    "Users",
+    {
+      User_id: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        primaryKey: true,
+      },
+      User_password: {
+        type: DataTypes.STRING(500),
+        allowNull: false,
+      },
+      User_name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      User_lastlogin: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      User_position: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 1,
+      },
     },
-    User_password: {
-      type: DataTypes.STRING(500),
-      allowNull: false
-    },
-    User_name: {
-      type: DataTypes.STRING(30),
-      allowNull: false
-    },
-    User_lastlogin: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    User_position: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 1
+    {
+      sequelize,
+      tableName: "Users",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "User_id" }],
+        },
+        {
+          name: "User_id_UNIQUE",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "User_id" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'Users',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "User_id" },
-        ]
-      },
-      {
-        name: "User_id_UNIQUE",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "User_id" },
-        ]
-      },
-    ]
-  });
+  );
+
+  Users.associate = function (models) {
+    Users.hasOne(models.Devices, {
+      foreignKey: "DEVICE_USER_ID",
+    });
+    Users.hasMany(models.Requests, {
+      foreignKey: "REG_USER_ID",
+    });
+  };
+
+  return Users;
 };
