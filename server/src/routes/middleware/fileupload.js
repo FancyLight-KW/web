bceedb0547de59045fef5742f6e508a5c85c3d30
jrhong95 <jrhong95@gmail.com>
2021-04-26 16:49:42 +0900

    경로문제 해결

--- server/src/routes/middleware/fileupload.js
+++ server/src/routes/middleware/fileupload.js
@@ -4,7 +4,8 @@ const moment = require("../../config/moment.config");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    console.log(__dirname + "/../../../uploads");
+    cb(null, __dirname + "/../../../uploads");
   },
   filename: (req, file, cb) => {
     cb(null, moment().format("YYYY-MM-DD_HH:mm:ss") + "-" + file.originalname);
