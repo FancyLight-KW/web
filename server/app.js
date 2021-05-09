3744d8b82263fcf80e2deeb688a6547d8019dea9
bingoring <ritch1530@gmail.com>
2021-05-09 21:03:33 +0900

    feature/update intent fix

--- server/app.js
+++ server/app.js
@@ -50,6 +50,7 @@ admin.initializeApp({
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(express.bodyParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, "public")));
