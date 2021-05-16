f29bfa6b97c46fd237ad03d7bc60721da1594e41
jrhong95 <jrhong95@gmail.com>
2021-05-16 22:30:57 +0900

    DB 정리 및 front 빌드

--- server/src/routes/chatbot/scenario/intents/intents.join.controller.js
+++
@@ -1,7 +0,0 @@
-const models = require("../../../../DB/models");
-
-exports.withPhrases = (req, res) => {};
-
-exports.withResponses = (req, res) => {};
-
-exports.withPhrasesAndResponses = (req, res) => {};
